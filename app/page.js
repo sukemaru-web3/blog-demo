0d4647c94ef73c63fdd0c9854f02eb21b5e2e719
sukemaru <sukemaru1006@gmail.com>
2023-02-17 15:29:27 +0900

    修正

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 import React from 'react';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 

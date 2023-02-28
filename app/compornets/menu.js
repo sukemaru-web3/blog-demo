9b23fc06441e7bf857dfd68fb6351bc58f822ede
sukemaru <sukemaru1006@gmail.com>
2023-02-28 21:38:06 +0900

    menuセクションまで完了

--- app/compornets/menu.js
+++ app/compornets/menu.js
@@ -7,19 +7,16 @@ export const Menu = () => {
   <section className="menu" id='menu'>
     <div className='container'>  
       <div className="row"> 
-        <div className="title w-auto col-sm-7 mx-auto text-center">
+        <div className="title mt-5 w-auto col-sm-7 mx-auto text-center">
           <h2 className=' border-bottom border-black border-3 '>Menu book</h2>
         </div>  
 
-        <div className='row menu_box my-4'>  
-            <div className='box col-md-6 p-4 '>
+        <div className='row menu_box my-4 justify-content-around'>  
+            <div className='box col-md-5 p-4 '>
                <img src="/menu_lunch.jpg"  />
-                 <p>おすすめは手作りホワイトソースのドリア</p>
-              </div>
-             <div className='box col-md-6 p-4 '>
+            </div>
+            <div className='box col-md-5 p-4 '>
                <img src="/menu_drink.jpg" />
-               
-                 <p >ドリンクは20種類以上</p>
             </div>
         </div>
       </div>

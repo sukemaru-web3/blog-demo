0772b25e61ac6579fab7036c585cc78d31dd72ee
sukemaru <sukemaru1006@gmail.com>
2023-03-02 13:23:58 +0900

    gameひとまず完成

--- app/compornets/game.js
+++ app/compornets/game.js
@@ -5,17 +5,22 @@ export const Game = () => {
     return (
         <>  
   <section className="game vh-100 bg-black  " id='game'>
-    <div className="row">
-      <div className="game_card   p-5 col-md-5">
-        <h1>ボードゲームカフェ</h1>
-        <h2>地頭をよくするツール</h2>
-        <div className="game_text    ">
-          <h2>ドミニオン</h2>
-          <p>カードのみで行うシンプルかつ奥深いゲーム。カードの効果を組み合わせ属州を集めよ</p>
-          <img src="/karou.png" alt="" />
-          <p>ゲームの紹介はアクセスのたび変わります</p>
-         </div>
+    <div className="row game_content">
+      <div className='text-center'>
+        <h1>LetsPlay</h1>
       </div>
+      <div className="game_card  p-5 col-md-4">
+        <div className=" game_text  ">
+          <h2>アナログゲーム紹介
+          </h2>
+          <div className='game_intro card card-effect  '>
+            <h3>ドミニオン</h3>
+            <p>カードのみで行うシンプルかつ奥深いゲーム。カードの効果を組み合わせ属州を集めよ</p>
+            <img src="/karou.png" alt="" />
+          </div>
+        </div> 
+      </div>
+      
     </div>
           </section> 
 

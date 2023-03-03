a61204ca5fb0aa396de66f726fa08a2f902ae5ce
sukemaru <sukemaru1006@gmail.com>
2023-03-03 14:43:11 +0900

    微調整

--- app/compornets/game.js
+++ app/compornets/game.js
@@ -1,49 +1,48 @@
-
-import React from 'react';
+import React from "react";
 
 export const Game = () => {
-    return (
-        <>  
-  <section className="game vh-100 bg-black  d-flex align-items-center" id='game'>
-    <div className='  d-flex  justify-content-end m-3'>
-      <div className=" game_content  ">
-        <div className='text-center   p-2 '>
-        <h1>various tools</h1>
-        <i  className="bi bi-arrow-down-square fs-3" ></i>
+  return (
+    <>
+      <section
+        className="game vh-100 bg-black  d-flex align-items-center"
+        id="game"
+      >
+        <div className="  d-flex  justify-content-end m-3">
+          <div className=" game_content  ">
+            <div className="text-center   p-2 ">
+              <h1>various tools</h1>
+              <i className="bi bi-arrow-down-square fs-3"></i>
 
-
-
-
-        <div  className="game_card  p-3 d-flex justify-content-around">
-          <div  className=" game_text  col-md-5  ">
-            <h2>アナログゲーム
-            </h2>
-            <div className='game_intro card card-effect  '>
-              <div className='mobile_box'>
-                <h3>ドミニオン</h3>
-                <p>カードのみで行うシンプルかつ奥深いゲーム。カードの効果を組み合わせ属州を集めよ</p>
+              <div className="game_card  p-3 d-flex justify-content-around">
+                <div className=" game_text  col-md-5  ">
+                  <h2>アナログゲーム</h2>
+                  <div className="game_intro card card-effect  ">
+                    <div className="mobile_box">
+                      <h3>ドミニオン</h3>
+                      <p>
+                        カードのみで行うシンプルかつ奥深いゲーム。カードの効果を組み合わせ属州を集めよ
+                      </p>
+                    </div>
+                    <img src="/karou.png" alt="" />
+                  </div>
+                </div>
+                <div className=" game_text  col-md-5 ">
+                  <h2>書籍</h2>
+                  <div className="game_intro card card-effect  ">
+                    <div className="mobile_box">
+                      <h3>伝え方が9割</h3>
+                      <p>
+                        上司への報告も、頼み事をする時も、物を売る時もぜんぶ言い方一つですって
+                      </p>
+                    </div>
+                    <img src="/karou.png" alt="" />
+                  </div>
+                </div>
               </div>
-              <img src="/karou.png" alt="" />
             </div>
-          </div> 
-          <div  className=" game_text  col-md-5 ">
-            <h2>書籍
-            </h2>
-            <div className='game_intro card card-effect  '>
-              <div className='mobile_box'>
-                  <h3>伝え方が9割</h3>
-                  <p>上司への報告も、頼み事をする時も、物を売る時もぜんぶ言い方一つですって</p>
-              </div>
-            <img src="/karou.png" alt="" />
-            </div>
-          </div> 
+          </div>
         </div>
-      </div>
-      </div>
-      
-    </div>
-          </section> 
-
-      
+      </section>
     </>
-  )}
+  );
+};
--- app/compornets/work.js
+++ app/compornets/work.js
@@ -1,34 +1,41 @@
-"use client"
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 
 export const Work = () => {
-
   const [toggle, setToggle] = useState(false);
   const handleClick = () => {
     setToggle(toggle === !true);
-  }
+  };
   const toggleCheck = toggle ? "open" : "";
 
-   return (
-  <>  
-      <section className="work vh-100  " id='work'>
+  return (
+    <>
+      <section className="work vh-100  " id="work">
         <div className="container">
-            <div className="title px-3 text-end ">
-              <h2 className='m-3'>コワーキングスペース</h2>
-              <p>仕事の打ち合わせや資格取得のために勉強する方など</p>
-              <p>色んな業種・年齢の方々がいます</p>
-            </div>
-        </div>
-        <div className='info d-flex justify-content-start ' >
-          {/* <div className="info_text"> */}
-            <div onClick={handleClick} className='d-flex  circle_btn align-items-center justify-content-center'>
-              <a className='fs-3 text-decoration-none text-white'>Infomation</a>
-            </div>
-            <div className={ "detail" + " " + (toggleCheck)}>
-              <p className={toggleCheck}>【料金に含まれているサービス】<br/>高速Wi-Fi/フリードリンク/各種コンセント/モニター/リングライト/ウェブカメラ/その他</p>
-            </div>
+          <div className="title px-3 text-end ">
+            <h2 className="m-3">コワーキングスペース</h2>
+            <p>仕事の打ち合わせや資格取得のために勉強する方など</p>
+            <p>色んな業種・年齢の方々がいます</p>
           </div>
+        </div>
+        <div className="info d-flex justify-content-start ">
+          {/* <div className="info_text"> */}
+          <div
+            onClick={handleClick}
+            className="d-flex  circle_btn align-items-center justify-content-center"
+          >
+            <a className="fs-3 text-decoration-none text-white">Infomation</a>
+          </div>
+          <div className={"detail" + " " + toggleCheck}>
+            <p className={toggleCheck}>
+              【料金に含まれているサービス】
+              <br />
+              高速Wi-Fi/フリードリンク/各種コンセント/モニター/リングライト/ウェブカメラ/その他
+            </p>
+          </div>
+        </div>
         {/* </div>      */}
-      </section> 
-  </>
-  )}
+      </section>
+    </>
+  );
+};
